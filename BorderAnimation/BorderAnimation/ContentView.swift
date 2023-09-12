559b9c6bd1e93eab837c653ff6ff5654842f7aff
Elaidzha1940 <eli080293@gmail.com>
2023-09-12 16:53:51 +0300

    BorderAnimation + added rotationEffect and Animation

--- BorderAnimation/BorderAnimation/ContentView.swift
+++ BorderAnimation/BorderAnimation/ContentView.swift
@@ -12,11 +12,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var rotation: CGFloat = 0.0
     var body: some View {
         
-        VStack {
+        ZStack {
             
+            RoundedRectangle(cornerRadius: 20, style: .continuous)
+                .frame(width: 250, height: 450)
+                .foregroundColor(Color("col"))
+                .rotationEffect(.degrees(rotation))
+            RoundedRectangle(cornerRadius: 20, style: .continuous)
+                .frame(width: 125, height: 460)
+                .foregroundStyle(LinearGradient(gradient: Gradient(colors: [Color.gray, Color.yellow]), startPoint: .top, endPoint: .bottom))
             
+        }
+        .preferredColorScheme(.dark)
+        .onAppear {
+            withAnimation(.linear(duration: 3).repeatForever(autoreverses: false)) {
+                rotation = 360
+            }
         }
     }
 }
