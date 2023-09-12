19903649f513a0d66b248c88c68e09ec8275b5f3
Elaidzha1940 <eli080293@gmail.com>
2023-09-12 16:36:37 +0300

    BorderAnimation

---
+++ BorderAnimation/BorderAnimation/BorderAnimationApp.swift
@@ -0,0 +1,21 @@
+//  /*
+//
+//  Project: BorderAnimation
+//  File: BorderAnimationApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 12.09.2023
+//
+//  Status: #in progress | Decorated
+//
+//  */
+
+import SwiftUI
+
+@main
+struct BorderAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BorderAnimation/BorderAnimation/ContentView.swift
@@ -0,0 +1,28 @@
+//  /*
+//
+//  Project: BorderAnimation
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 12.09.2023
+//
+//  Status: #in progress | Decorated
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+            
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
