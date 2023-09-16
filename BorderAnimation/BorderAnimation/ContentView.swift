5c68144363a25b3e5db32aa1c8de047ee934fe53
Elaidzha1940 <eli080293@gmail.com>
2023-09-16 21:19:22 +0300

    BorderAnimation

--- BorderAnimation/BorderAnimation/ContentView.swift
+++ BorderAnimation/BorderAnimation/ContentView.swift
@@ -20,12 +20,16 @@ struct ContentView: View {
             
             RoundedRectangle(cornerRadius: 20, style: .continuous)
                 .frame(width: 260, height: 440)
-                .foregroundStyle(LinearGradient(gradient: Gradient(colors: [Color.gray.opacity(0.8), Color.yellow.opacity(0.7)]), startPoint: .top, endPoint: .bottom))                
+                .foregroundColor(Color("col"))
             
             RoundedRectangle(cornerRadius: 20, style: .continuous)
-                .frame(width: 130, height: 450)
+                .frame(width: 130, height: 500)
                 .foregroundStyle(LinearGradient(gradient: Gradient(colors: [Color.yellow.opacity(0.7), Color.gray.opacity(0.8)]), startPoint: .top, endPoint: .bottom))
                 .rotationEffect(.degrees(rotation))
+                .mask {
+                    RoundedRectangle(cornerRadius: 20, style: .continuous)
+                    .frame(width: 256, height: 336)
+                }
          
         }
         .preferredColorScheme(.dark)
